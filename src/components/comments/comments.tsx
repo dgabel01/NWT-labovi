85bda60261914fb58e39539dd9855f84f635624b
Dominik Gabela <gabela.dominik@gmail.com>
2023-11-14 18:26:23 +0100

    Lab_6

--- src/components/comments/comments.tsx
+++ src/components/comments/comments.tsx
@@ -1,14 +1,19 @@
+//funkcionalna komponenta lab6
+
 import * as React from 'react';
 import styles from "./comments.module.css";
 
+
 export interface CommentsProps {
+
 }
 
 export interface CommentsState {
     comments: Comment[];
-    isLoading?: boolean;
-    error?: any;
+    isLoading: boolean;
+    error: any;
 }
+
 
 interface Comment {
     postId: number;
@@ -18,64 +23,62 @@ interface Comment {
     body: string;
 }
 
-class Comments extends React.Component<CommentsProps, CommentsState> {
-    constructor(props: CommentsProps) {
-        super(props);
-        this.state = {
-            comments: [],
-            isLoading: true, 
-        };
-    }
+const Comments2:React.FC<CommentsProps> = () =>{
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/comments?_limit=10') 
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
+    const[comments, setComments] = React.useState<Comment[]>([]);   
+    const[isLoading, setLoading] = React.useState<boolean>(false);
+
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch("https://jsonplaceholder.typicode.com/comments");
+
+                if (!response.ok) {
+                    throw new Error("Something went wrong ...");
                 }
-                throw new Error("Something went wrong");
-            })
-            .then((comments) => {
-                this.setState({
-                    comments,
-                    isLoading: false, 
-                });
-            })
-            .catch((error: any) => {
-                console.log(error);
-            });
-    }
 
-    render() {
-        return (
-            <>
+                const data = await response.json();
+                setComments(data.slice(0, 15));
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, []); 
+    return(
+        <>
                 <div className="titleContainer">
-                    <h1 className={styles.title}>Komentari :</h1>
+                <h1 className={styles.title}>Komentari :</h1>
                 </div>
                 <div className={styles.commentsContainer}>
-                    {this.state.isLoading ? ( 
-                        <div>Loading...</div>
-                    ) : (
-                        this.state.comments.map((comment) => ( 
-                            <div className={styles.commentContainer} key={comment.id}>
-                                <div className={styles.commentPersonal}>
-                                    <div className={styles.commentEmail}>
-                                        <strong>E-mail : {comment.email}</strong> 
+                    {   isLoading
+                        ? "Komentari se učitavaju..." 
+                            : comments.map((comment: Comment) => (
+                                <div key={comment.id} className={styles.commentContainer}>
+                                    <div className={styles.commentPersonal}>
+                                        <div className={styles.commentEmail}>
+                                            <strong>E-mail : </strong>
+                                            {comment.email}
+                                        </div>
+                                        <img
+                                            src="https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?b=1&k=6&m=1223671392&s=612x612&w=0&h=5VMcL3a_1Ni5rRHX0LkaA25lD_0vkhFsb1iVm1HKVSQ="
+                                            className={styles.commentImage}
+                                            alt={comment.name}
+                                        ></img>
                                     </div>
-                                    <img
-                                        src="https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?b=1&k=6&m=1223671392&s=612x612&w=0&h=5VMcL3a_1Ni5rRHX0LkaA25lD_0vkhFsb1iVm1HKVSQ="
-                                        className={styles.commentImage}
-                                        alt={comment.name} 
-                                    ></img>
+                                    <div className={styles.commentBody}>{comment.body}</div>
                                 </div>
-                                <div className={styles.commentBody}>{comment.body}</div> 
-                            </div>
-                        ))
-                    )}
+                            ))}
                 </div>
             </>
-        );
-    }
+
+    );
 }
 
-export default Comments;
+
+
+export default Comments2;
--- src/components/park/park.tsx
+++ src/components/park/park.tsx
@@ -1,89 +1,76 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./park.module.css";
 import parksData from "../../json/parksData.json";
+
 interface ParkProps {
   match: {
     params: {
       id: string;
-    },
-  },
+    };
+  };
 }
-
- interface ParkState {
-  park: ParkData;  
- }
 
 interface ParkData {
   id: number;
   name: string;
   description: string;
   posts: ParkPost[];
-} 
+}
 
 interface ParkPost {
   title: string;
   text: string;
   image: string;
-} 
-
-class Park extends Component<ParkProps, ParkState> {
-
-  constructor(props: ParkProps) {
-    super(props);
-    const newId = +this.props.match?.params?.id;
-    const newPark = parksData.parks?.filter((park) => park.id === newId)[0]
-    this.state = {
-      park: newPark,
-    } 
-  }
-
-  extractParkData(){
-    const newId = +this.props.match?.params?.id;
-    const newPark = parksData.parks?.filter((park) => park.id === newId)[0]
-    this.setState({
-      park: newPark,
-    })
-  } 
-
-  componentDidUpdate(prevProps: ParkProps, prevState: ParkState) {
-      if(prevProps.match?.params?.id != this.props.match?.params?.id){
-        this.extractParkData()
-      }
-  } 
-
-  render() {
-    return (
-      <>
-        <div className={"titleContainer"}>
-          <h1 className={styles.title}>{this.state.park.name}</h1>
-        </div>
-        <div className={styles.parkContainer}>
-          <p>{this.state.park.description}</p>
-          <div className={styles.posts}>
-            {this.state.park.posts.map((post, index) => {
-              const image = (
-                <img
-                  src={post.image}
-                  className={styles.postImage}
-                  alt={post.title}
-                ></img>
-              );
-              return (
-                <div className={styles.postContainer} key={index}>
-                  {index % 2 === 1 ? image : ""}
-                  <div className={styles.postText}>
-                    <h2 className={styles.postTitle}>{post.title}</h2>
-                    <p>{post.text}</p>
-                  </div>
-                  {index % 2 === 0 ? image : ""}
-                </div>
-              );
-            })}
-          </div>
-        </div> 
-      </>
-    );
-  }
 }
 
-export default Park;
+const Park2: React.FC<ParkProps> = (props) => {
+  const [park, setPark] = useState<ParkData | null>(null);
+
+  const extractParkData = () => {
+    const newId = +props.match?.params?.id;
+    const newPark = parksData.parks?.find((park) => park.id === newId) || null;
+    setPark(newPark);
+  };
+
+  useEffect(() => {
+    extractParkData();
+  }, [props.match?.params?.id]);
+
+  return (
+    <>
+      {park && (
+        <>
+          <div className={"titleContainer"}>
+            <h1 className={styles.title}>{park.name}</h1>
+          </div>
+          <div className={styles.parkContainer}>
+            <p>{park.description}</p>
+            <div className={styles.posts}>
+              {park.posts.map((post, index) => {
+                const image = (
+                  <img
+                    src={post.image}
+                    className={styles.postImage}
+                    alt={post.title}
+                  ></img>
+                );
+                return (
+                  <div className={styles.postContainer} key={index}>
+                    {index % 2 === 1 ? image : ""}
+                    <div className={styles.postText}>
+                      <h2 className={styles.postTitle}>{post.title}</h2>
+                      <p>{post.text}</p>
+                    </div>
+                    {index % 2 === 0 ? image : ""}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Park2;
